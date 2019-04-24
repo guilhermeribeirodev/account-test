ba978a38d9325e1316e4b8900c79b2485bbbf947
guilhermeribeiro <guilhermeribeirodev@gmail.com>
2019-04-24 17:34:10 +0100

    implemented equals to match account - tests pass

--- src/main/kotlin/com/example/account/domain/Account.kt
+++ src/main/kotlin/com/example/account/domain/Account.kt
@@ -6,4 +6,22 @@ import javax.persistence.Id
 
 @Entity
 class Account(var sortCode : String, @Id var accNumber : String, var balance : BigDecimal) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Account
+
+        if (sortCode != other.sortCode) return false
+        if (accNumber != other.accNumber) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = sortCode.hashCode()
+        result = 31 * result + accNumber.hashCode()
+        return result
+    }
 }
