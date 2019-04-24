2c7e0049bb58cc7fbfabe2ade00eea99d63502a5
guilhermeribeiro <guilhermeribeirodev@gmail.com>
2019-04-24 17:20:13 +0100

    implemented equals to match by account - passed

--- src/main/kotlin/com/example/account/domain/AccountHolder.kt
+++ src/main/kotlin/com/example/account/domain/AccountHolder.kt
@@ -5,4 +5,22 @@ import javax.persistence.Id
 
 @Entity
 class AccountHolder(var name : String, var sortCode : String, @Id var accNumber : String) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as AccountHolder
+
+        if (sortCode != other.sortCode) return false
+        if (accNumber != other.accNumber) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = sortCode.hashCode()
+        result = 31 * result + accNumber.hashCode()
+        return result
+    }
 }
